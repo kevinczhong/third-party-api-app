d001529bf9891af84fc30c53352863cbac26db9c
Kevin Zhong <kvnzhong@gmail.com>
2022-06-06 12:17:30 -0700

    Added API encryption.

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -1,6 +1,6 @@
 class NumbersController < ApplicationController
   def index
-    response = HTTP.get("https://api.veriphone.io/v2/verify?phone=#{params[:number]}&key=ED49E43E873B4D798883DD7E06A174E3")
+    response = HTTP.get("https://api.veriphone.io/v2/verify?phone=#{params[:number]}&key=#{Rails.application.credentials.phone_api[:api_key]}")
     render json: response.parse(:json)
   end
 end
